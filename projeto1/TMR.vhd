03e1b9ee23eaf76423f89499ad2e3a79b66a227a
LPR Sattra <lg.lpr2429@gmail.com>
2020-11-11 22:45:04 -0300

    identação

--- projeto1/TMR.vhd
+++ projeto1/TMR.vhd
@@ -2,8 +2,8 @@
 -- Design: TMR
 -- Entity: TMR
 -- Author: Diogo & George
--- Rev.  : 1.0
--- Date  : 11/05/2020
+-- Rev. : 1.0
+-- Date : 11/05/2020
 ------------------------------------------------
 
 library ieee;
@@ -11,50 +11,48 @@ use ieee.std_logic_1164.all;
 use work.all;
 
 entity TMR is
-port ( 
-       i_CLK : in  std_logic; -- clock 
-		 i_CLR : in  std_logic; -- clear
-       i_A   : in  std_logic_vector(15 downto 0);  -- data input
-       i_B   : in  std_logic_vector(15 downto 0);  -- data input
-       i_C   : in  std_logic_vector(15 downto 0);  -- data input
-       o_A   : out std_logic_vector(15 downto 0));  -- data output
+  port (
+    i_CLK : in std_logic; -- clock
+    i_CLR : in std_logic; -- clear
+    i_A   : in std_logic_vector(15 downto 0); -- data input
+    i_B   : in std_logic_vector(15 downto 0); -- data input
+    i_C   : in std_logic_vector(15 downto 0); -- data input
+    o_A   : out std_logic_vector(15 downto 0)); -- data output
 end TMR;
 
-
 architecture arch_1 of TMR is
+
   signal r_A, r_B, r_c : std_logic_vector(15 downto 0);
-  signal r_o_A : std_logic_vector(15 downto 0);
-  
+  signal r_o_A         : std_logic_vector(15 downto 0);
+ 
 begin
-  
   -- sincronismo para o circuito
   process (i_CLK, i_CLR) is
   begin
     if (i_CLR = '1') then
-	   r_A <= "0000000000000000";
+      r_A <= "0000000000000000";
       r_B <= "0000000000000000";
-      r_c <= "0000000000000000";		
+      r_c <= "0000000000000000"; 
     elsif (rising_edge(i_CLK)) then
-	   r_A <= i_A;
+      r_A <= i_A;
       r_B <= i_B;
-      r_c <= i_C;
-		o_A <= r_o_A;
-	 end if;
+      r_C <= i_C;
+      o_A <= r_o_A;
+    end if;
   end process;
-                            
+ 
   -- logica do votador
-  process(i_CLR, r_A, r_B, r_c) is
-  begin
-    
-    for i in 0 to 15 loop       -- loop para percorrer os bits
-      if(r_A(i) = r_B(i) or r_A(i) = r_c(i)) then  
-                                -- compara os bits do canal A e B
-        r_o_A(i)  <= r_A(i);    -- se for igual atribui A como saida
-      else                      -- se não
-        r_o_A(i)  <= r_B(i);    -- atribui B como saida
-      end if;
-    end loop;
-    
-  end process;
-  
+  process (i_CLR, r_A, r_B, r_C) is
+    begin
+      for i in 0 to 15 loop -- loop para percorrer os bits
+        if (r_A(i) = r_B(i) or r_A(i) = r_C(i)) then 
+          -- compara os bits do canal A e B
+          r_o_A(i) <= r_A(i); -- se for igual atribui A como saida
+        else -- se não
+          r_o_A(i) <= r_B(i); -- atribui B como saida
+        end if;
+      end loop;
+ 
+    end process;
+ 
 end arch_1;
--- projeto1/TMR_tb.vhd
+++ projeto1/TMR_tb.vhd
@@ -1,108 +1,108 @@
 ------------------------------------------------
--- Design: mux_e_demux
--- Entity: tb_mux_e_demux
+-- Design: tb_TMR
+-- Entity: TMR_tb
 -- Author: Diogo
--- Rev.  : 1.0
--- Date  : 03/24/2020
+-- Rev. : 1.0
+-- Date : 03/24/2020
 ------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
- 
+
 entity TMR_tb is
--- empty
-end TMR_tb; 
+  -- empty
+end TMR_tb;
 
 architecture arch_1 of TMR_tb is
 
--- DUT component
-component TMR is
-port (        
-		 i_CLK : in  std_logic; -- clock 
-		 i_CLR : in  std_logic; -- clear
-       i_A   : in  std_logic_vector(15 downto 0);  -- data input
-       i_B   : in  std_logic_vector(15 downto 0);  -- data input
-       i_C   : in  std_logic_vector(15 downto 0);  -- data input
-       o_A   : out std_logic_vector(15 downto 0));  -- data output
-		 		 
-end component;
+  -- DUT component
+  component TMR is
+    port (
+      i_CLK : in std_logic; -- clock
+      i_CLR : in std_logic; -- clear
+      i_A   : in std_logic_vector(15 downto 0); -- data input
+      i_B   : in std_logic_vector(15 downto 0); -- data input
+      i_C   : in std_logic_vector(15 downto 0); -- data input
+    o_A   : out std_logic_vector(15 downto 0)); -- data output
+ 
+  end component;
 
-signal w_CLK, w_CLR : std_logic;
-signal w_i_A, w_i_B, w_i_C, w_o_A: std_logic_vector(15 downto 0);
+  signal w_CLK, w_CLR               : std_logic;
+  signal w_i_A, w_i_B, w_i_C, w_o_A : std_logic_vector(15 downto 0);
 
-constant c_CLK_PERIOD : time := 2 ns;
+  constant c_CLK_PERIOD             : time := 2 ns;
 
 begin
-
   -- Connect DUT
-  u_DUT: TMR port map(
-    i_CLK   => w_CLK,
-    i_CLR   => w_CLR,
-    i_A     => w_i_A,
-    i_B     => w_i_B,
-    i_C     => w_i_C,
-    o_A     => w_o_A);
+  u_DUT : TMR
+  port map(
+    i_CLK => w_CLK, 
+    i_CLR => w_CLR, 
+    i_A   => w_i_A, 
+    i_B   => w_i_B, 
+    i_C   => w_i_C, 
+    o_A   => w_o_A
+  );
 
-  
+ 
   -- clock
-  p_CLK: process
+  p_CLK : process
   begin
     w_CLK <= '1';
-    wait for c_CLK_PERIOD/2;  
+    wait for c_CLK_PERIOD/2; 
     w_CLK <= '0';
-    wait for c_CLK_PERIOD/2;  
+    wait for c_CLK_PERIOD/2; 
   end process p_CLK;
-  
+ 
   -- reset
-  p_CLR: process
+  p_CLR : process
   begin
     w_CLR <= '0';
-    wait for c_CLK_PERIOD;  
+    wait for c_CLK_PERIOD; 
     w_CLR <= '1';
-    wait for c_CLK_PERIOD;  
+    wait for c_CLK_PERIOD; 
     w_CLR <= '0';
     wait; -- for the end of simulation
   end process p_CLR;
-  
-  
+ 
+ 
   -- main process
   process
-  begin
-	 
-	 -- wait for reset
-	 w_i_A       <= "0000000000000000";
-    w_i_B       <= "0000000000000000";
-    w_i_C       <= "0000000000000000";
-	 wait for 4*c_CLK_PERIOD;
-	 
-    -- canal A = B
-    w_i_A       <= "0000111100001111";
-    w_i_B       <= "0000111100001111";
-    w_i_C       <= "1111000011110000";
-    wait  for 2*c_CLK_PERIOD;
-    assert(w_o_A="0000111100001111") report "Fail @ 000" severity error;
-    
-	 -- canal B = C
-    w_i_A       <= "0000111100001111";
-    w_i_B       <= "1111000011110000";
-    w_i_C       <= "1111000011110000";
-    wait  for 2*c_CLK_PERIOD;
-    assert(w_o_A="1111000011110000") report "Fail @ 001" severity error;    
-	 
-	 -- teste para diferentes bits
-    w_i_A       <= "0000111100001111";
-    w_i_B       <= "1011000011110000";
-    w_i_C       <= "1110101010101010";
-    wait  for 2*c_CLK_PERIOD;
-    assert(w_o_A="1010101010101010") report "Fail @ 001" severity error;    
-  
+    begin
+      -- wait for reset
+      w_i_A <= "0000000000000000";
+      w_i_B <= "0000000000000000";
+      w_i_C <= "0000000000000000";
+      wait for 4 * c_CLK_PERIOD;
+ 
+      -- canal A = B
+      w_i_A <= "0000111100001111";
+      w_i_B <= "0000111100001111";
+      w_i_C <= "1111000011110000";
+      wait for 2 * c_CLK_PERIOD;
+      assert(w_o_A = "0000111100001111") report "Fail @ 000" severity error;
+ 
+      -- canal B = C
+      w_i_A <= "0000111100001111";
+      w_i_B <= "1111000011110000";
+      w_i_C <= "1111000011110000";
+      wait for 2 * c_CLK_PERIOD;
+      assert(w_o_A = "1111000011110000") report "Fail @ 001" severity error; 
+ 
+      -- teste para diferentes bits
+      w_i_A <= "0000111100001111";
+      w_i_B <= "1011000011110000";
+      w_i_C <= "1110101010101010";
+      wait for 2 * c_CLK_PERIOD;
+      assert(w_o_A = "1010101010101010") report "Fail @ 001" severity error; 
+ 
 
-    -- Clear inputs
-    w_i_A       <= "0000000000000000";
-    w_i_B       <= "0000000000000000";
-    w_i_C       <= "0000000000000000";
-    assert false report "Test done." severity note;
-    
-    wait;
-  end process;
+      -- Clear inputs
+      w_i_A <= "0000000000000000";
+      w_i_B <= "0000000000000000";
+      w_i_C <= "0000000000000000";
+      assert false report "Test done." severity note;
+ 
+      wait;
+    end process;
 end arch_1;
